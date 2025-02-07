eb68740351aeed44cad03a2ae06d4a9896d56971
Paulo Gama <DreamerJPMG@gmail.com>
2025-02-06 22:38:17 -0300

    Update SuporteApp.py
    v1.4

--- SuporteApp.py
+++ SuporteApp.py
@@ -5,6 +5,7 @@ from tkinter import simpledialog, scrolledtext, filedialog
 import json
 import winsound
 
+
 class SupportApp:
     def __init__(self, root):
         self.root = root
@@ -13,7 +14,8 @@ class SupportApp:
         # Carregar configurações
         self.load_config()
 
-        self.root.configure(bg=self.bg_color)  # Cor de fundo da janela principal
+        # Cor de fundo da janela principal
+        self.root.configure(bg=self.bg_color)
 
         # Carregar imagem de fundo
         self.load_bg_image()
@@ -22,6 +24,7 @@ class SupportApp:
         self.load_texts()
         self.create_widgets()
         self.create_menu()
+        self.create_notepad_widget()
 
         # Salvar tamanho e posição da janela
         self.root.bind("<Configure>", self.save_window_size)
@@ -31,20 +34,25 @@ class SupportApp:
         self.save_config()
 
     def load_config(self):
-        config_path = os.path.join(os.path.dirname(sys.executable), "config.txt")
+        config_path = os.path.join(
+            os.path.dirname(sys.executable), "config.txt")
         if os.path.exists(config_path):
             try:
                 with open(config_path, "r") as file:
                     config = json.load(file)
-                    self.bg_image_path = config.get("bg_image_path", "background.png")
+                    self.bg_image_path = config.get(
+                        "bg_image_path", "background.png")
                     self.sound_enabled = config.get("sound_enabled", True)
                     self.dark_mode = config.get("dark_mode", False)
                     self.bg_color = config.get("bg_color", "#2C3E50")
                     self.btn_fg_color = config.get("btn_fg_color", "#ECF0F1")
                     self.btn_bg_color = config.get("btn_bg_color", "#E74C3C")
-                    self.edit_btn_bg_color = config.get("edit_btn_bg_color", "#3498DB")
-                    self.show_edit_buttons = config.get("show_edit_buttons", True)
-                    self.window_size = config.get("window_size", "800x600+100+100 ")
+                    self.edit_btn_bg_color = config.get(
+                        "edit_btn_bg_color", "#3498DB")
+                    self.show_edit_buttons = config.get(
+                        "show_edit_buttons", True)
+                    self.window_size = config.get(
+                        "window_size", "800x600+100+100")
             except json.JSONDecodeError:
                 self.bg_image_path = "background.png"
                 self.sound_enabled = True
@@ -68,7 +76,8 @@ class SupportApp:
 
         # Pro curar automaticamente o diretório de imagens de fundo
         if not os.path.exists(self.bg_image_path):
-            self.bg_image_path = os.path.join(os.path.dirname(sys.executable), "background.png")
+            self.bg_image_path = os.path.join(
+                os.path.dirname(sys.executable), "background.png")
             if not os.path.exists(self.bg_image_path):
                 self.bg_image_path = self.select_bg_image_dir()
 
@@ -76,7 +85,8 @@ class SupportApp:
         self.root.geometry(self.window_size)
 
     def save_config(self):
-        config_path = os.path.join(os.path.dirname(sys.executable), "config.txt")
+        config_path = os.path.join(
+            os.path.dirname(sys.executable), "config.txt")
         config = {
             "bg_image_path": self.bg_image_path,
             "sound_enabled": self.sound_enabled,
@@ -96,20 +106,23 @@ class SupportApp:
             if os.path.isabs(self.bg_image_path):
                 self.bg_image = tk.PhotoImage(file=self.bg_image_path)
             else:
-                self.bg_image = tk.PhotoImage(file=os.path.join(os.path.dirname(sys.executable), self.bg_image_path))
+                self.bg_image = tk.PhotoImage(file=os.path.join(
+                    os.path.dirname(sys.executable), self.bg_image_path))
         except tk.TclError:
             self.bg_image_path = self.select_bg_image_dir()
             self.bg_image = tk.PhotoImage(file=self.bg_image_path)
 
     def select_bg_image_dir(self):
-        file_path = filedialog.askopenfilename(title="Selecione a imagem de fundo", filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
+        file_path = filedialog.askopenfilename(title="Selecione a imagem de fundo", filetypes=[
+                                               ("Image files", "*.png")])
         if file_path:
             return file_path
         else:
-            return "background.png "  # Retorna um valor padrão caso o usuário cancele a seleção
+            return "background.png "  # Retorna um valor padrão caso o usuário cancele a sele ç ã o
 
     def create_widgets(self):
-        self.canvas = tk.Canvas(self.root, width=self.bg_image.width(), height=self.bg_image.height())
+        self.canvas = tk.Canvas(
+            self.root, width=self.bg_image.width(), height=self.bg_image.height())
         self.canvas.pack(fill="both", expand=True)
         self.canvas.create_image(0, 0, image=self.bg_image, anchor="nw")
 
@@ -122,12 +135,15 @@ class SupportApp:
         column = 0
         row = 0
         for idx, (text, resumo) in enumerate(self.texts):
-            button = tk.Button(button_frame, text=resumo, command=lambda t=text: self.copy_to_clipboard(t), bg=self.btn_bg_color, fg=self.btn_fg_color, relief="flat")
+            button = tk.Button(button_frame, text=resumo, command=lambda t=text: self.copy_to_clipboard(
+                t), bg=self.btn_bg_color, fg=self.btn_fg_color, relief="flat")
             button.grid(row=row, column=column, padx=5, pady=5, sticky="nsew")
 
-            edit_button = tk.Button(button_frame, text="Editar", command=lambda idx=idx: self.open_edit_window(idx), bg=self.edit_btn_bg_color, fg=self.btn_fg_color, relief="flat")
+            edit_button = tk.Button(button_frame, text="Editar", command=lambda idx=idx: self.open_edit_window(
+                idx), bg=self.edit_btn_bg_color, fg=self.btn_fg_color, relief="flat")
             if self.show_edit_buttons:
-                edit_button.grid(row=row+1, column=column, padx=5, pady=5, sticky="nsew")
+                edit_button.grid(row=row+1, column=column,
+                                 padx=5, pady=5, sticky="nsew")
             else:
                 edit_button.grid_forget()
 
@@ -142,29 +158,36 @@ class SupportApp:
 
         theme_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Temas", menu=theme_menu)
-        theme_menu.add_command(label="Alterar Plano de Fundo", command=self.change_bg_image)
+        theme_menu.add_command(
+            label="Alterar Plano de Fundo", command=self.change_bg_image)
 
         self.sound_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Som", menu=self.sound_menu)
-        self.sound_menu.add_command(label="Desativar Som de Clique" if self.sound_enabled else "Ativar Som de Clique", command=self.toggle_sound)
+        self.sound_menu.add_command(
+            label="Desativar Som de Clique" if self.sound_enabled else "Ativar Som de Clique", command=self.toggle_sound)
 
         self.view_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Visual", menu=self.view_menu)
-        self.view_menu.add_command(label="Desativar Modo Noturno" if self.dark_mode else "Ativar Modo Noturno", command=self.toggle_dark_mode)
-        self.view_menu.add_command(label="Editar Cores", command=self.edit_colors)
-        self.view_menu.add_command(label="Ocultar Botões de Edição" if self.show_edit_buttons else "Exibir Botões de Edição", command=self.toggle_edit_buttons)
+        self.view_menu.add_command(
+            label="Desativar Modo Noturno" if self.dark_mode else "Ativar Modo Noturno", command=self.toggle_dark_mode)
+        self.view_menu.add_command(
+            label="Editar Cores", command=self.edit_colors)
+        self.view_menu.add_command(
+            label="Ocultar Botões de Edição" if self.show_edit_buttons else "Exibir Botões de Edição", command=self.toggle_edit_buttons)
 
     def copy_to_clipboard(self, text):
         self.root.clipboard_clear()
         self.root.clipboard_append(text)
         if self.sound_enabled:
-            self.root.after(1, lambda: winsound.PlaySound(os.path.join(os.path.dirname(sys.executable), "click.wav"), winsound.SND_FILENAME | winsound.SND_ASYNC))  # SOM
+            self.root.after(1, lambda: winsound.PlaySound(os.path.join(os.path.dirname(
+                sys.executable), "click.wav"), winsound.SND_FILENAME | winsound.SND_ASYNC))  # SOM
 
     def open_edit_window(self, idx):
         edit_window = tk.Toplevel(self.root)
         edit_window.title("Editar Texto")
 
-        text_box = scrolledtext.ScrolledText(edit_window, wrap=tk.WORD, width=50, height=15)
+        text_box = scrolledtext.ScrolledText(
+            edit_window, wrap=tk.WORD, width=50, height=15)
         text_box.pack(padx=10, pady=10)
         text_box.insert(tk.END, self.texts[idx][0])
 
@@ -195,12 +218,13 @@ class SupportApp:
     def toggle_edit_buttons(self):
         self.show_edit_buttons = not self.show_edit_buttons
         new_label = "Ocultar Botões de Edição" if self.show_edit_buttons else "Exibir Botões de Edição"
-        self.view_menu.entryconfig (3, label=new_label)
+        self.view_menu.entryconfig(3, label=new_label)
         self.save_config()
         self.refresh_gui()
 
     def load_texts(self):
-        texts_path = os.path.join(os.path.dirname(sys.executable), "texts.json")
+        texts_path = os.path.join(
+            os.path.dirname(sys.executable), "texts.json")
         if os.path.exists(texts_path):
             with open(texts_path, "r", encoding="utf-8") as file:
                 self.texts = json.load(file)
@@ -210,12 +234,14 @@ class SupportApp:
             ]
 
     def save_texts(self):
-        texts_path = os.path.join(os.path.dirname(sys.executable), "texts.json")
+        texts_path = os.path.join(
+            os.path.dirname(sys.executable), "texts.json")
         with open(texts_path, "w", encoding="utf-8") as file:
             json.dump(self.texts, file, ensure_ascii=False, indent=4)
 
     def change_bg_image(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
+        file_path = filedialog.askopenfilename(
+            filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
         if file_path:
             self.bg_image_path = file_path
             self.bg_image = tk.PhotoImage(file=file_path)
@@ -227,6 +253,62 @@ class SupportApp:
             widget.destroy()
         self.create_widgets()
         self.create_menu()
+        self.create_notepad_widget()
+
+    def create_notepad_widget(self):
+        self.notepad_frame = tk .Frame(self.root, bg=self.bg_color)
+        self.notepad_frame.pack(fill="both", expand=True)
+
+        self.notepad_text = scrolledtext.ScrolledText(
+            self.notepad_frame, width=50, height=10, wrap=tk.WORD)
+        self.notepad_text.pack(padx=10, pady=10, fill="both", expand=True)
+
+        self.notepad_toolbar = tk.Frame(self.notepad_frame, bg=self.bg_color)
+        self.notepad_toolbar.pack(fill="x", side="top")
+
+        self.bold_button = tk.Button(self.notepad_toolbar, text="Negrito",
+                                     command=lambda: self.notepad_text.tag_add("bold", "sel.first", "sel.last"))
+        self.bold_button.pack(side="left")
+
+        self.italic_button = tk.Button(self.notepad_toolbar, text="Itálico",
+                                       command=lambda: self.notepad_text.tag_add("italic", "sel.first", "sel.last"))
+        self.italic_button.pack(side="left")
+
+        self.underline_button = tk.Button(self.notepad_toolbar, text="Sublinhado",
+                                          command=lambda: self.notepad_text.tag_add("underline", "sel.first", "sel.last"))
+        self.underline_button.pack(side="left")
+
+        self.separator_button = tk.Button(
+            self.notepad_toolbar, text="Adicionar separação", command=self.add_separator)
+        self.separator_button.pack(side="left")
+
+        self.save_notepad_button = tk.Button(
+            self.notepad_toolbar, text="Salvar Alterações", command=self.save_notepad)
+        self.save_notepad_button.pack(side="right")
+
+        self.notepad_text.tag_config("bold", font=("Helvetica", "12", "bold"))
+        self.notepad_text.tag_config(
+            "italic", font=("Helvetica", "12", "italic"))
+        self.notepad_text.tag_config(
+            "underline", font=("Helvetica", "12", "underline"))
+
+        self.load_notepad()
+
+    def load_notepad(self):
+        notepad_path = os.path.join(
+            os.path.dirname(sys.executable), "notepad.txt")
+        if os.path.exists(notepad_path):
+            with open(notepad_path, "r", encoding="utf-8") as file:
+                self.notepad_text.insert(tk.END, file.read())
+
+    def save_notepad(self):
+        notepad_path = os.path.join(
+            os.path.dirname(sys.executable), "notepad.txt")
+        with open(notepad_path, "w", encoding="utf-8") as file:
+            file.write(self.notepad_text.get("1.0", tk.END))
+
+    def add_separator(self):
+        self.notepad_text.insert(tk.END, "\n______________________\n")
 
     def edit_colors(self):
         edit_window = tk.Toplevel(self.root)
@@ -261,8 +343,10 @@ class SupportApp:
             self.refresh_gui()
             edit_window.destroy()
 
-        save_button = tk.Button(edit_window, text="Salvar", command=save_colors)
-        save_button.pack(pady=5)
+        save_button = tk.Button(
+            edit_window, text="Salvar", command=save_colors)
+        save_button.pack(pady=5, side="bottom")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
